e9d775d22f12058f17e13e5a9cc9f926d68e8689
mnk-19900910 <wccf19900910@yahoo.co.jp>
2020-05-19 16:04:36 +0900

    温泉の件数表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(5)
+    @posts_num = Post.includes(:user)
   end
 
   def new
