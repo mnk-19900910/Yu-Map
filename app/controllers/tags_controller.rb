213c2204027e776617d096fc596978c765e66368
mnk-19900910 <wccf19900910@yahoo.co.jp>
2020-05-22 14:04:06 +0900

    確認

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,19 @@
 class TagsController < ApplicationController
   def new
+    @tags = Tag.new
   end
 
   def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
+  private
+  def tag_params
+    params.require(:tag).permit(:text, post_ids: [])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_034208) do
-
+ActiveRecord::Schema.define(version: 2020_05_22_023501) do
+  
   create_table "kuchikomis", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
