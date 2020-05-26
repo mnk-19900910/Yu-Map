a34aa564b9e454a1a10914d8aa4315fa1da6a251
mnk-19900910 <wccf19900910@yahoo.co.jp>
2020-05-26 19:13:17 +0900

    heroku10

--- db/migrate/20200521025934_remove_tag_from_posts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTagFromPosts < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :posts, :tag_id, :string
-  end
-end
--- db/migrate/20200522023501_remove_id_from_kuchikomis.rb
+++
@@ -1,6 +0,0 @@
-class RemoveIdFromKuchikomis < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :kuchikomis, :user_id, :string
-    remove_column :kuchikomis, :post_id, :string
-  end
-end
