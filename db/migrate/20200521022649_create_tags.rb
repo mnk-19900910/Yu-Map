23d40d4837d76418c8ad268dc735969bfd07027f
mnk-19900910 <wccf19900910@yahoo.co.jp>
2020-05-26 19:09:25 +0900

    heroku9

--- db/migrate/20200521022649_create_tags.rb
+++
@@ -1,9 +0,0 @@
-class CreateTags < ActiveRecord::Migration[5.2]
-  def change
-    create_table :tags do |t|
-      t.string :text, null: false
-      t.index :text, unique: true
-      t.timestamps
-    end
-  end
-end
